7ce947f29e8570d5d242202d4f9c95fb4ae39173
unknown <ivucicev@gmail.com>
2016-04-04 20:39:41 +0200

    profile picture of undefied fixed

--- APIv2/api/controllers/UserController.js
+++ APIv2/api/controllers/UserController.js
@@ -9,22 +9,24 @@ module.exports = {
     getProfilePicture: function(req, res) {
         User.findOne({id: req.params.id}).exec(function(err, user) {
             if (err) return res.send(404, 'Not found');
-            var ext = '';
-            if (user.profilePicture.indexOf('png')) {
-                ext = 'png';
-            } else if (user.profilePicture.indexOf('jpeg')) {
-                ext = 'jpeg';
-            } else if (user.profilePicture.indexOf('jpg')) {
-                ext = 'jpg';
-            } else if (user.profilePicture.indexOf('gif')) {
-                ext = 'gif';
-            }
-            var img = new Buffer(user.profilePicture.replace(/^data:image\/(png|gif|jpeg|jpg);base64,/,''), 'base64');
-            res.writeHead(200, {
-                'Content-Type': 'image/' + ext,
-                'Content-Length': img.length
-            });
-            res.end(img);
+            if (user.profilePicture) {
+                var ext = '';
+                if (user.profilePicture.indexOf('png')) {
+                    ext = 'png';
+                } else if (user.profilePicture.indexOf('jpeg')) {
+                    ext = 'jpeg';
+                } else if (user.profilePicture.indexOf('jpg')) {
+                    ext = 'jpg';
+                } else if (user.profilePicture.indexOf('gif')) {
+                    ext = 'gif';
+                }
+                var img = new Buffer(user.profilePicture.replace(/^data:image\/(png|gif|jpeg|jpg);base64,/,''), 'base64');
+                res.writeHead(200, {
+                    'Content-Type': 'image/' + ext,
+                    'Content-Length': img.length
+                });
+                res.end(img);
+            } else return res.send(404, 'Not found');
         });
     },
     getGalleryImage: function(req, res) {
